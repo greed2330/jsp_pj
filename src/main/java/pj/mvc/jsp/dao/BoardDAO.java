d1fa74ac579430bafb3b1c29c52817ff961b7f64
greed2330 <oscar2003hu@gmail.com>
2025-07-23 15:06:38 +0900

    feat: 에러픽스 및 컨트롤러 내용 추가

--- src/main/java/pj/mvc/jsp/dao/BoardDAO.java
+++ src/main/java/pj/mvc/jsp/dao/BoardDAO.java
@@ -1,5 +1,31 @@
 package pj.mvc.jsp.dao;
 
+import pj.mvc.jsp.dto.BoardDTO;
+
 public interface BoardDAO {
+	//게시글 목록
+	public BoardDTO boardList(int start, int end);
+	
+	//게시글 갯수 구하기
+	public int boardCnt();
+	
+	//조회수 증가
+	public void plusReadCnt(int num);
+	
+	//게시글 상세 처리
+	public BoardDTO getBoardDetail(int num);
+	
+	//게시글 수정삭제 버튼 클릭 시 - 비밀번호 인증처리
+	
+	//게시글 수정 처리
+	
+	//게시글 삭제 처리
+	
+	//게시글 작성 처리
+	
+	//댓글 작성 처리
+	
+	//댓글 목록
+	
 
 }
--- src/main/java/pj/mvc/jsp/service/BoardServiceImpl.java
+++ src/main/java/pj/mvc/jsp/service/BoardServiceImpl.java
@@ -1,5 +1,75 @@
 package pj.mvc.jsp.service;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class BoardServiceImpl implements BoardService{
+
+	//[게시글 목록]
+	@Override
+	public void boardListAction(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//[게시글 상세 처리]
+	@Override
+	public void boardDetailAction(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//[게시글 수정삭제 버튼 클릭 시 - 비밀번호 인증처리]
+	@Override
+	public int password_chkAction(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	//[게시글 수정 처리]
+	@Override
+	public void boardUpdateAction(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//[게시글 삭제 처리]
+	@Override
+	public void boardDeleteAction(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//[게시글 작성 처리]
+	@Override
+	public void boardInsertAction(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//[댓글 작성 처리]
+	@Override
+	public void commentAddaction(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//[댓글 목록]
+	@Override
+	public void commentListAction(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
